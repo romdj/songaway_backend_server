93a6e596f69b7150c30f2e0898637bf4dfe17a1b
Romain Lussier <romdj_123@hotmail.com>
2020-05-16 21:56:58 +0200

    adding first bits of express server components

--- src/service/server.mjs
+++ src/service/server.mjs
@@ -1,19 +1,106 @@
-import {readdirSync, lstatSync} from 'fs';
-import Hapi from 'hapi';
+/**
+ * NPM Module dependencies.
+ */
+import express from 'express';
+const trackRoute = express.Router();
+import multer from 'multer';
 
-// import { main, artist, album, item } from '../domain/music';
-// import * as images from '../domain/images';
-// import * as videos from '../domain/videos';
-import * as music from '../domain/music';
-console.log('ok');
-const server = Hapi.server({
-  load: {
-    sampleInterval: 1000
-  },
-  port: 1221
+import mongodb from 'mongodb';
+import { MongoClient } from 'mongodb';
+import { ObjectID } from 'mongodb';
+
+/**
+ * NodeJS Module dependencies.
+ */
+const { Readable } = require('stream');
+
+/**
+ * Create Express server && Express Router configuration.
+ */
+const app = express();
+app.use('/tracks', trackRoute);
+
+/**
+ * Connect Mongo Driver to MongoDB.
+ */
+let db;
+MongoClient.connect('mongodb://localhost/trackDB', (err, database) => {
+  if (err) {
+    console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
+    process.exit(1);
+  }
+  db = database;
 });
-music.forEach(item => server.route(item));
 
-server.start();
+/**
+ * GET /tracks/:trackID
+ */
+trackRoute.get('/:trackID', (req, res) => {
+  try {
+    var trackID = new ObjectID(req.params.trackID);
+  } catch (err) {
+    return res.status(400).json({ message: "Invalid trackID in URL parameter. Must be a single String of 12 bytes or a string of 24 hex characters" });
+  }
+  res.set('content-type', 'audio/mp3');
+  res.set('accept-ranges', 'bytes');
 
-console.log("Yoo! Let's hear some music y'all!");
+  let bucket = new mongodb.GridFSBucket(db, {
+    bucketName: 'tracks'
+  });
+
+  let downloadStream = bucket.openDownloadStream(trackID);
+
+  downloadStream.on('data', (chunk) => {
+    res.write(chunk);
+  });
+
+  downloadStream.on('error', () => {
+    res.sendStatus(404);
+  });
+
+  downloadStream.on('end', () => {
+    res.end();
+  });
+});
+
+/**
+ * POST /tracks
+ */
+trackRoute.post('/', (req, res) => {
+  const storage = multer.memoryStorage()
+  const upload = multer({ storage: storage, limits: { fields: 1, fileSize: 6000000, files: 1, parts: 2 } });
+  upload.single('track')(req, res, (err) => {
+    if (err) {
+      return res.status(400).json({ message: "Upload Request Validation Failed" });
+    } else if (!req.body.name) {
+      return res.status(400).json({ message: "No track name in request body" });
+    }
+
+    let trackName = req.body.name;
+
+    // Covert buffer to Readable Stream
+    const readableTrackStream = new Readable();
+    readableTrackStream.push(req.file.buffer);
+    readableTrackStream.push(null);
+
+    let bucket = new mongodb.GridFSBucket(db, {
+      bucketName: 'tracks'
+    });
+
+    let uploadStream = bucket.openUploadStream(trackName);
+    let id = uploadStream.id;
+    readableTrackStream.pipe(uploadStream);
+
+    uploadStream.on('error', () => {
+      return res.status(500).json({ message: "Error uploading file" });
+    });
+
+    uploadStream.on('finish', () => {
+      return res.status(201).json({ message: "File uploaded successfully, stored under Mongo ObjectID: " + id });
+    });
+  });
+});
+
+app.listen(3005, () => {
+  console.log("App listening on port 3005!");
+});
